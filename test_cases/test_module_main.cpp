4d66a1d159e8fc2f6e60addb5612e3952a27d035
peilin.shao <peilin.shao@intchains.com>
2026-03-30 16:49:21 +0800

    feat: add opencv into test_moduel_main.cpp

--- test_cases/test_module_main.cpp
+++ test_cases/test_module_main.cpp
@@ -202,24 +202,26 @@ int main(int argc, char* argv[]) {
         return all_ok ? 0 : 1;
     }
 
-    // OpenCV 算术运算 (ADD, ABSDIFF, etc.) - 使用 OpenCV 回调
-    if (pipeline_configs.size() == 2 &&
-        pipeline_configs[0].consumer_type.opencv.enable &&
-        pipeline_configs[0].consumer_type.opencv.op_type != WorkerConfig::ConsumerTypeConfig::OpencvType::OpType::NONE) {
-        
-        std::cout<<"start opencv"<<std::endl;
-        // 设置 OpenCV 回调到两个 config
-        auto opencv_ctx = std::make_shared<OpenCVCallbackContext>();
-        opencv_ctx->config = pipeline_configs[0].consumer_type.opencv;
-        opencv_ctx->worker1_name = "hw_decoder";
-        opencv_ctx->worker2_name = "sw_decoder";
+    // OpenCV 算术运算 (ADD, ABSDIFF, etc.)
+    if (pipeline_configs.size() >= 1 &&
+        pipeline_configs[0].consumer_type.opencv.enable) {
 
-        if (!opencv_ctx->openComparator()) {
-            std::cerr << "Error: Failed to open BufferComparator for OpenCV\n" << std::endl;
-            return 1;
+        using OpType = WorkerConfig::ConsumerTypeConfig::OpencvType::OpType;
+        bool is_arithmetic_op = (pipeline_configs[0].consumer_type.opencv.op_type == OpType::ADD ||
+                                 pipeline_configs[0].consumer_type.opencv.op_type == OpType::ABSDIFF ||
+                                 pipeline_configs[0].consumer_type.opencv.op_type == OpType::ADD_WEIGHTED ||
+                                 pipeline_configs[0].consumer_type.opencv.op_type == OpType::BITWISE_AND ||
+                                 pipeline_configs[0].consumer_type.opencv.op_type == OpType::BITWISE_OR ||
+                                 pipeline_configs[0].consumer_type.opencv.op_type == OpType::BITWISE_XOR ||
+                                 pipeline_configs[0].consumer_type.opencv.op_type == OpType::BITWISE_NOT);
+
+        if ((is_arithmetic_op || compare_enabled) && pipeline_configs.size() == 2) {
+            auto result = test::ExecuteMode::compare(pipeline_configs, flags, test_name + " (OPENCV)");
+            consumer::BufferConsumerService::printResult(test_name, result);
+            return result.success ? 0 : 1;
         }
-
-        auto result = test::ExecuteMode::compare(pipeline_configs, flags, test_name + " (OPENCV)");
+        std::cout << "opencv single start" << std::endl;
+        auto result = test::ExecuteMode::single(pipeline_configs[0], flags, test_name);
         consumer::BufferConsumerService::printResult(test_name, result);
         return result.success ? 0 : 1;
     }
@@ -263,7 +265,6 @@ int main(int argc, char* argv[]) {
     }
 
     // SINGLE
-    std::cout << "start opencv end" <<std::endl;
     auto result = test::ExecuteMode::single(pipeline_configs[0], flags, test_name);
     consumer::BufferConsumerService::printResult(test_name, result);
     return result.success ? 0 : 1;
