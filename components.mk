6f6bffe94aaf4b2756bb530bfacba3d4078d968a
xiafeng.luo <xiafeng.luo@intchains.com>
2026-06-30 18:16:49 +0800

    chore: bump version 2.82 -> 2.83

--- components.mk
+++ components.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-COMPONENTS_VERSION = 2.82
+COMPONENTS_VERSION = 2.83
 COMPONENTS_SITE = ../packages/components
 COMPONENTS_SITE_METHOD = local
 COMPONENTS_INSTALL_STAGING = YES
