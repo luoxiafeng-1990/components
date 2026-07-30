0035c1a83616256d30c89f36f22ddcfda3c72a11
xiafeng.luo <xiafeng.luo@intchains.com>
2026-07-30 10:37:35 +0800

    Revert "chore: bump version 2.88 → 2.89"
    This reverts commit b48d3a0b3842a023c59404982f24a74555714d45.

--- components.mk
+++ components.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-COMPONENTS_VERSION = 2.89
+COMPONENTS_VERSION = 2.88
 COMPONENTS_SITE = ../packages/components
 COMPONENTS_SITE_METHOD = local
 COMPONENTS_INSTALL_STAGING = YES
